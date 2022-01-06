7521700087198ed91a985f30c1833bd0e1a21312
Filipe Moreno <y8.filipe@gmail.com>
2022-01-06 19:09:35 -0300

    fix: column 'youtube_url' added

--- src/database/migrations/20210110204927-configuracoes.js
+++ src/database/migrations/20210110204927-configuracoes.js
@@ -50,6 +50,10 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
+      youtube_url: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
       created_at: {
         type: Sequelize.DATE,
         allowNull: false,
